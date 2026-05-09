27a3829bb811d80c5e154f29448bbe2626a5187d
Samuel James <samuelsuperguy@gmail.com>
2026-05-09 14:09:39 +0100

    Starting the project a fresh; May God help me these time, Activate my Genius Brain

--- dynevest_project/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Wallet
-from .models import Wallet, MiningPlan, Transaction  # Add MiningPlan here!
-
-admin.site.register(Wallet)
-admin.site.register(MiningPlan)
-admin.site.register(Transaction) # Register it here
--- dynevest_project/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('plans/', views.plans_view, name='plans'),
-    path('transactions/', views.transactions_view, name='transactions'),
-    path('settings/', views.settings_view, name='settings'),
-]
